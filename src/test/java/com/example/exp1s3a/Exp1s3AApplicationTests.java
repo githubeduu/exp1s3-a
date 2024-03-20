018d59f4984d4aed20b4edbe06e7d6fb4d01bc87
Eduardo <edo.carvajal@hotmail.com>
2024-03-20 17:40:13 -0300

    first commit

---
+++ src/test/java/com/example/exp1s3a/Exp1s3AApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exp1s3a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exp1s3AApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
